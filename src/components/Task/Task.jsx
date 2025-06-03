8212e38f601e0a0d1499a24b90c31fe009d33a69
alisher22062005 <sh4ripov.alisher@yandex.kz>
2025-06-03 16:01:23 +0500

    feat(responsive): fix the error in MenuDafault

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -5,14 +5,15 @@ export default function Task({ task, doneTask, trashListisClicked }) {
     <>
       <div className="flex gap-[0.7rem] sm:ml-[7%] mt-[1%] sm:w-[35%] xs:w-[90%] xs:ml-[2%] xs:mt-[1%] hover:bg-[#E4E6E7]  p-[0.3rem]">
         <img className="sm:ml-[0.5rem]" src={icon}></img>
-        <input
-          className=" flex justify-center "
-          type="checkbox"
-          checked={task.done}
-          onChange={() => {
-            doneTask(task);
-          }}
-        ></input>
+        <div className=" flex justify-center items-center ">
+          <input
+            type="checkbox"
+            checked={task.done}
+            onChange={() => {
+              doneTask(task);
+            }}
+          ></input>
+        </div>
 
         <div className={task.done ? "line-through" : ""}>{task.name}</div>
       </div>
